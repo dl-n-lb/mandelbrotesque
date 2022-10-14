94caeca513ef58406fc66c7fae449cff3c610dd4
matt <myounger009@gmail.com>
2022-10-14 15:15:01 +0100

    Setup

---
+++ source/main.cxx
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <complex>
+#include "types/type_alias.hxx"
+#define RAYLIB_WINDOW_IMPL
+#define RAYLIB_RENDERER_IMPL
+#include "graphics/raylib_window/raylib_window.hx"
+
+int main(int, char**) {
+  printf("Hello World");
+  return 0;
+}
